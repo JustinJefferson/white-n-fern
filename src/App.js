3a006b309987ab162a2aee1a6e1c390660b5cd33
JustinJefferson <justleland@gmail.com>
2020-02-17 14:45:35 -0500

    Created props for Sidebar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
@@ -9,49 +9,49 @@ import Workshop from './pages/Workshop';
 
 const list = [
   'Gallery',
-  'Workshp',
+  'Workshop',
   'Flowers',
   'About'
 ];
 
-function App() {
-  return (
-    <div className="App">
-      <div className="Sidebar">
-        <Sidebar pages={list} open={true} />
+class App extends Component {
+  render () {
+    return (
+      <div className="App"> 
+        <Router>
+          <Sidebar pages={list} open={true} />
+          <h1>White & Fern</h1>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/gallery">
+              <Gallery />
+            </Route>
+            <Route path="/workshop">
+              <Workshop />
+            </Route>
+            <Route path="/flowers"></Route>
+            <Route path="/about"></Route>
+          </Switch>
+        </Router>
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header> */}
       </div>
-      <h1>White & Fern</h1>
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/gallery">
-            <Gallery />
-          </Route>
-          <Route path="/workshop">
-            <Workshop />
-          </Route>
-          <Route path="/flowers"></Route>
-          <Route path="/about"></Route>
-        </Switch>
-      </Router>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
