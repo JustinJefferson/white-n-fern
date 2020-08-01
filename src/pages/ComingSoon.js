908e63c657f63cd47be4cbf68e144414b4b8e0ce
Justin Jefferson <justleland@gmail.com>
2020-08-01 18:21:31 -0400

    Coming Soon splash page build

--- src/pages/ComingSoon.js
+++ src/pages/ComingSoon.js
@@ -3,9 +3,12 @@ import './ComingSoon.css';
 
 export default function ComingSoon() {
     return (
-        <div className="ComingSoon">
-            <h1>White & Fern</h1>
-            <h3>Coming Soon...</h3>
+        <div>
+            {/* <div className="FullCover"></div>
+            <div className="ComingSoon">
+                <h1>White & Fern</h1>
+                <h3>Coming Soon...</h3>
+            </div> */}
         </div>
     )
 }
