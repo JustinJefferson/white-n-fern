b6677ab0bfeb442219a3b09b8dc0dd808948c24e
Justin Jefferson <justleland@gmail.com>
2020-08-09 21:31:14 -0400

    Added routing withing gallery page

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,5 +1,55 @@
 import React, { Component } from 'react';
+import { Switch, Route, Link, useParams, useRouteMatch } from 'react-router-dom';
 import { GalleryItem, MockGalleryList } from '../models/GalleryItem'
+
+const SingleItem = (props) => {
+    const { list } = props;
+    const { itemId } = useParams();
+    const item = list[itemId];
+    return (
+        <div>
+            <h3>{item.title}</h3>
+                <p>{item.description}</p>
+                <p>{/*This should be an a link*/}
+                    Read More...
+                </p>
+        </div>
+    )
+}
+
+const ListItems = (props) => {
+    const list = props.list;
+    const { url } = useRouteMatch();
+    return (
+        <div>
+            {list.map(item =>
+                <div>
+                    {item.getImg()}
+                    <h3>{item.title}</h3>
+                    <p>{item.description}</p>
+                    <Link to={`${url}/${item.id}`}>Read More...</Link>
+                </div> 
+            )}
+        </div>
+    )
+}
+
+const Routing = (props) => {
+    const { path } = useRouteMatch();
+    const list = props.list;
+    return (
+        <div>
+            <Switch>
+                <Route exact path={path}>
+                    <ListItems list={list}/>
+                </Route>
+                <Route path={`${path}/:itemId`}>
+                    <SingleItem list={list}/>
+                </Route>
+            </Switch>
+        </div>
+    )
+}
 
 class Gallery extends Component{
     constructor(props) {
@@ -12,19 +62,9 @@ class Gallery extends Component{
 
     render() {
         const {list} = this.state;
+        
         return (
-            <div>
-                {list.map(item =>
-                   <div>
-                       {item.getImg()}
-                       <h3>{item.title}</h3>
-                       <p>{item.description}</p>
-                       <p>{/*This should be an a link*/}
-                            Read More...
-                       </p>
-                   </div> 
-                )}
-            </div>
+            <Routing list={list}/>
         );
     }
 
